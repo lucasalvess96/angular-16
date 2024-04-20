b2f4fb1fe018a103195e839018a364bf29c26205
lucasalvess96 <lucasalves.57@hotmail.com>
2024-04-20 00:28:41 -0300

    feat: implementando componente page-not-found

---
+++ src/app/features/page-not-found/page-not-found.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [PageNotFoundComponent],
+  imports: [CommonModule, PageNotFoundRoutingModule],
+})
+export class PageNotFoundModule {}
