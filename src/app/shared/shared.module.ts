3e9ff2fb941babe163e9503e407b362e33ff8ccc
lucasalvess96 <lucasalves.57@hotmail.com>
2024-04-20 00:32:58 -0300

    feat: implementando modulos de dependencias

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  IConfig,
+  NgxMaskDirective,
+  NgxMaskPipe,
+  provideEnvironmentNgxMask,
+} from 'ngx-mask';
+import { ToastrModule } from 'ngx-toastr';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+
+const maskConfig: Partial<IConfig> = {
+  validation: false,
+};
+
+@NgModule({
+  declarations: [],
+  providers: [provideEnvironmentNgxMask(maskConfig)],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    NgxMaskDirective,
+    NgxMaskPipe,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+  ],
+  exports: [
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxMaskDirective,
+    NgxMaskPipe,
+  ],
+})
+export class SharedModule {}
