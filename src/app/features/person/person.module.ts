6daf0735d9183ec2e84db7f22462b51703e7faf2
lucasalvess96 <lucasalves.57@hotmail.com>
2024-05-31 09:59:12 -0300

    fix: implementando fxLayoutModule no pacote person

--- src/app/features/person/person.module.ts
+++ src/app/features/person/person.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PersonCreateComponent } from './pages/person-create/person-create.component';
 import { PersonComponent } from './pages/person/person.component';
@@ -9,6 +10,6 @@ import { PersonRoutingModule } from './person-routing.module';
 
 @NgModule({
   declarations: [PersonComponent, PersonCreateComponent],
-  imports: [CommonModule, SharedModule, PersonRoutingModule, HttpClientModule],
+  imports: [CommonModule, SharedModule, PersonRoutingModule, HttpClientModule, FlexLayoutModule],
 })
 export class PersonModule {}
