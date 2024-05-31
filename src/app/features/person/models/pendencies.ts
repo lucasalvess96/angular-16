cb67b02a7232eb1accf99907223a1d88f0cfa775
lucasalvess96 <lucasalves.57@hotmail.com>
2024-05-31 16:35:50 -0300

    fix: ajuste no model person

---
+++ src/app/features/person/models/pendencies.ts
@@ -0,0 +1,3 @@
+export interface Pendencies {
+  pendeciesID: number;
+}
--- src/app/features/person/models/person.ts
+++ src/app/features/person/models/person.ts
@@ -1,7 +1,11 @@
+import { Pendencies } from './pendencies';
+
 export interface Person {
   name: string;
 
   age: string;
 
   cpf: string;
+
+  pendencies: Pendencies;
 }
