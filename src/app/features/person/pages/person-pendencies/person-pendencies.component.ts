13d58cfe162b2b6ad399ba24b99d4510aa79c689
lucasalvess96 <lucasalves.57@hotmail.com>
2024-06-07 01:14:02 -0300

    feat: implentando detalhes da empresa

--- src/app/features/person/pages/person-pendencies/person-pendencies.component.ts
+++ src/app/features/person/pages/person-pendencies/person-pendencies.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+interface Empresa {
+  nome: string;
+  endereco: string;
+  telefone: string;
+  email: string;
+}
+
 @Component({
   selector: 'app-person-pendencies',
   templateUrl: './person-pendencies.component.html',
-  styleUrls: ['./person-pendencies.component.scss']
+  styleUrls: ['./person-pendencies.component.scss'],
 })
 export class PersonPendenciesComponent {
-
+  mostrarDetalhes: boolean = false;
 }
